216c2352f76cb2211d318aed0d60df9c307d4a19
ash2code <ashoksoma95@gmail.com>
2024-05-03 11:09:06 +0000

    variable tf file added

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+    description = "ami id info"
+    type = string
+    default = "ami-04b70fa74e45c3917"
+}
+
+variable "instance_type" {
+    description = "instance type"
+    type = string
+    default = "t2.micro"
+}
