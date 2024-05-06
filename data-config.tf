5d656df56b56cc198066e066ab42c0d91809f162
ash2code <144456748+ash2code@users.noreply.github.com>
2024-05-06 13:53:26 +0530

    Update data-config.tf

--- data-config.tf
+++ data-config.tf
@@ -1,7 +1,7 @@
 data "aws_instance" "myawsinstance" {
     filter {
         name = "image-id"
-        values = ["ami-04b70fa74e45c3917"]
+        values = ["ami-0e001c9271cf7f3b9"]
     }
     filter {
         name   = "instance-state-name"
